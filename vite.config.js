6524a60d84fca421fc4d3ff0c8183a3172929db9
Arnab <arnavmallick12006@gmail.com>
2025-06-14 13:13:52 +0530

    some fix vercel2

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://eco-commerce-2vxl.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+}) 
